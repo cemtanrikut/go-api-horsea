50126520629db9d1d0806a60509d47859a714b5a
cemtanrikut <cemtanrikut@outlook.com>
2022-01-08 22:34:14 +0300

    Add mongo db handler and test file

---
+++ db/mongo.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"context"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+var ctx context.Context
+var collection *mongo.Collection
+var router *mux.Router
+
+func MongoClient(collection_name string) (*mux.Router, context.Context, *mongo.Client, *mongo.Collection) {
+	router = mux.NewRouter()
+	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ = mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://admin:LCtfPjhpm1am7HRd@sandbox.0sac2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	collection = client.Database("horsea_db").Collection(collection_name) //user_collection
+
+	return router, ctx, client, collection
+}
--- main.go
+++ main.go
@@ -4,27 +4,24 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"time"
 
 	api "github.com/cemtanrikut/go-api-horsea/api/user"
+	db "github.com/cemtanrikut/go-api-horsea/db"
 	"github.com/cemtanrikut/go-api-horsea/helper"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/gorilla/mux"
 )
 
 var client *mongo.Client
 var ctx context.Context
-var collection *mongo.Collection
+var userCollection *mongo.Collection
+var router *mux.Router
 
 func main() {
 	log.Println("Starting the application")
 
-	router := mux.NewRouter()
-	ctx, _ = context.WithTimeout(context.Background(), 10*time.Second)
-	client, _ = mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://admin:LCtfPjhpm1am7HRd@sandbox.0sac2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
-	collection = client.Database("horsea_db").Collection("user_collection")
+	router, ctx, client, userCollection = db.MongoClient("user_collection")
 
 	router.HandleFunc("/api/user/login", LogIn).Methods(http.MethodPost)
 	router.HandleFunc("/api/user/signup", SignUp).Methods(http.MethodPost)
@@ -37,35 +34,35 @@ func main() {
 }
 
 func LogIn(w http.ResponseWriter, r *http.Request) {
-	result := api.LogIn(w, r, client, ctx, collection)
+	result := api.LogIn(w, r, client, ctx, userCollection)
 	byteRes := helper.JsonMarshal(result)
 	w.Write(byteRes)
 }
 func SignUp(w http.ResponseWriter, r *http.Request) {
-	result := api.SignUp(w, r, client, collection)
+	result := api.SignUp(w, r, client, userCollection)
 	byteRes := helper.JsonMarshal(result)
 	w.Write(byteRes)
 }
 func GetUser(w http.ResponseWriter, r *http.Request) {
 	email := mux.Vars(r)["email"]
-	result := api.GetUser(email, w, r, client, collection)
+	result := api.GetUser(email, w, r, client, userCollection)
 	byteRes := helper.JsonMarshal(result)
 	w.Write(byteRes)
 }
 func GetUsers(w http.ResponseWriter, r *http.Request) {
-	result := api.GetUsers(client, w, r, collection)
+	result := api.GetUsers(client, w, r, userCollection)
 	byteRes := helper.JsonMarshal(result)
 	w.Write(byteRes)
 }
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
-	result := api.UpdateUser(w, r, collection)
+	result := api.UpdateUser(w, r, userCollection)
 	byteRes := helper.JsonMarshal(result)
 	w.Write(byteRes)
 }
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
 	v := mux.Vars(r)
 	email := v["email"]
-	result := api.DeleteUser(email, w, r, collection)
+	result := api.DeleteUser(email, w, r, userCollection)
 	byteRes := helper.JsonMarshal(result)
 	w.Write(byteRes)
 }
