fcbfafc9d365e49cb01ef8d818b27e166a2f9da4
cemtanrikut <cemtanrikut@outlook.com>
2022-01-08 23:17:04 +0300

    Add test functions without scenario

--- api/user/user_test.go
+++ api/user/user_test.go
@@ -7,3 +7,27 @@ import (
 func TestSignUp(t *testing.T) {
 
 }
+
+func TestLogIn(t *testing.T) {
+
+}
+
+func TestGetUser(t *testing.T) {
+
+}
+
+func TestGetUsers(t *testing.T) {
+
+}
+
+func TestUpdateUser(t *testing.T) {
+
+}
+
+func TestDeleteUser(t *testing.T) {
+
+}
+
+func TestCheckEmail(t *testing.T) {
+
+}
