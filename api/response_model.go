768e75833c27fba69bebe08210d3573d71b97808
cemtanrikut <cemtanrikut@outlook.com>
2022-01-09 00:18:19 +0300

    Add SignUp test

--- api/response_model.go
+++ api/response_model.go
@@ -1,8 +1,7 @@
 package api
 
 type Response struct {
-    Data         string `json:"data"`
-    StatusCode   int    `json:"statusCode"`
-    ErrorMessage string `json:"errorMessage"`
+	Data         string `json:"data"`
+	StatusCode   int    `json:"statusCode"`
+	ErrorMessage string `json:"errorMessage"`
 }
-
--- api/user/user_test.go
+++ api/user/user_test.go
@@ -1,10 +1,38 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/cemtanrikut/go-api-horsea/db"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSignUp(t *testing.T) {
+	t.Parallel()
+	var user User
+	user.FirstName = "Cem"
+	user.LastName = "Tanrikut"
+	user.Email = "cem@gmail.com"
+	user.Password = "1234"
+
+	reqBodyBytes := new(bytes.Buffer)
+	json.NewEncoder(reqBodyBytes).Encode(user)
+
+	r, _ := http.NewRequest("POST", "/api/user/signup", io.Reader(reqBodyBytes))
+	w := httptest.NewRecorder()
+
+	w.Body.Write(reqBodyBytes.Bytes())
+
+	_, _, client, collection := db.MongoClient("user_collection")
+
+	response := SignUp(w, r, client, collection)
+
+	assert.Equal(t, http.StatusOK, response.StatusCode)
 
 }
 
